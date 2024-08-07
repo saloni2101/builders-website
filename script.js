2757a52f9570091056b7c7e4b4f5368e45827ecb
saloni2101 <156669166+saloni2101@users.noreply.github.com>
2024-08-07 16:07:38 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,141 @@
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active'); 
+};
+
+document.querySelector('#info-btn').onclick = () =>{
+   contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick = () =>{
+   contactInfo.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+   contactInfo.classList.remove('active');
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+var swiper = new Swiper(".amenities-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+   640: {
+     slidesPerView: 3,
+   },
+   768: {
+     slidesPerView: 4,
+   },
+   1000: {
+     slidesPerView: 5,
+   },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".logo-slider", {
+   loop:true,
+   grabCursor:true,
+   spaceBetween: 20,
+   breakpoints: {
+      450: {
+         slidesPerView: 2,
+       },
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1000: {
+        slidesPerView: 5,
+      },
+   },
+});
